c5e69297fa032b3860a454ff7989b3f6f41a2f39
Chechel01 <m1chellin4869@gmail.com>
2025-05-22 11:04:15 -0300

    Add files via upload

---
+++ Atv-time-main/Atv-time-main/91164-ds-react-spring-mysql/api/src/main/java/com/example/aula/model/Usuario.java
@@ -0,0 +1,96 @@
+package com.example.aula.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Usuario {
+//    private String nome;
+//    private String sexo;
+//    private int idade;
+//    private double altura;
+//   private double peso;
+//    private String posicao;
+//    private int numCamisa;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Nome é obrigatório.")
+    private String nome;
+
+    @NotBlank(message = "Sexo é obrigatório.")
+    private String sexo;
+
+    @NotBlank(message = "Idade é obrigatório.")
+    private int idade;
+
+    @NotBlank(message = "Altura é obrigatório.")
+    private double altura;
+
+    @NotBlank(message = "Peso é obrigatório.")
+    private double peso;
+
+    @NotBlank(message = "Posição é obrigatório.")
+    private String posicao;
+
+    @NotBlank(message = "Número da camisa é obrigatória.")
+    private int numCamisa;
+
+    public Usuario() {
+    }
+
+    public Usuario(Long id, String nome, String sexo, int idade, double altura, double peso, String posicao, int numCamisa) {
+        this.id = id;
+        this.nome = nome;
+        this.sexo = sexo;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+        this.posicao = posicao;
+        this.numCamisa = numCamisa;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public @NotBlank(message = "Nome é obrigatório.") String getNome() {
+        return nome;
+    }
+    public void setNome(@NotBlank(message = "Nome é obrigatório.") String nome) {this.nome = nome;}
+
+    public @NotBlank(message = "Idade é obrigatório.") int getIdade() {
+        return idade;
+    }
+    public void setIdade(@NotBlank(message = "Idade é obrigatório.") int idade) {this.idade = idade;}
+
+    public @NotBlank(message = "Sexo é obrigatório.") String getSexo() {return sexo; }
+    public void setSexo(@NotBlank(message = "Sexo é obrigatório.") String sexo) {this.sexo = sexo;}
+
+    public @NotBlank(message = "Altura é obrigatório.") double getAltura() {
+        return altura;
+    }
+    public void setAltura(@NotBlank(message = "Altura é obrigatório.") double altura) {this.altura = altura;}
+
+    public @NotBlank(message = "Peso é obrigatório.") double getPeso() {
+        return peso;
+    }
+    public void setPeso(@NotBlank(message = "Peso é obrigatório.") double peso) {this.peso = peso;}
+
+    public @NotBlank(message = "Posição é obrigatório.") String getPosicao() {return posicao;}
+    public void setPosicao(@NotBlank(message = "Posição é obrigatório.") String posicao) {
+        this.posicao = posicao;
+    }
+
+    public @NotBlank(message = "Número da camisa é obrigatória.") int getNumCamisa() {return numCamisa;}
+    public void setNumCamisa(@NotBlank(message = "Número da camisa é obrigatória.") int numCamisa) {this.numCamisa = numCamisa;}
+}
